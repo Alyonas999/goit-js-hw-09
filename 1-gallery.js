65316f2f757b60222786e24a06c0d2b3bdbb16f6
Alyonas999 <oranchuka@gmail.com>
2025-05-27 19:44:12 +0000

    Deploying to gh-pages from @ Alyonas999/goit-js-hw-09@b1ee3255ec5f225f910616e9ccf36887ed4da17a 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as e}from"./assets/vendor-CgTBfC_f.js";const a=document.querySelector(".gallery");a.insertAdjacentHTML("beforeend",imageMarkup);new e(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
